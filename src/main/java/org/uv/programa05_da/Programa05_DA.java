db9fb41bb6d0a545bc99b1eaf31598e33ce48894
Adriel <asp.security@protonmail.com>
2023-04-24 10:33:36 -0400

    Uploading repository

---
+++ src/main/java/org/uv/programa05_da/Programa05_DA.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package org.uv.programa05_da;
+
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.hibernate.Session;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import static org.hibernate.criterion.Projections.id;
+
+
+public class Programa05_DA {
+
+    public static void main(String[] args) {
+//        Session session = HibernateUtil.getSession();
+        DAOEmpleado dao=new DAOEmpleado();
+        Empleado empleado= new Empleado();
+        empleado.setClave(14);
+        empleado.setNombre("nodal");
+        empleado.setDireccion("av. 1234");
+        empleado.setTelefono("4444");
+        
+        dao.create(empleado);
+        empleado.setClave(2); 
+        empleado.setNombre("edward");
+        empleado.setDireccion("xing");
+        empleado.setTelefono("111"); 
+        dao.delete(1);
+        System.out.println("se elimino");
+        
+        
+        //actualiza
+        empleado.setClave(13); 
+        empleado.setNombre("alphone");
+        empleado.setDireccion("atoyac");
+        empleado.setTelefono("222"); 
+       // dao.update(3);
+        System.out.println("se actualizo");
+        
+   
+
+    }
+}
